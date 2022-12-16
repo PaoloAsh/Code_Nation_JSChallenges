ca87632b64935f4ad953ec36581f10a1aa962fca
PaoloAsh <paolo.ash2022@gmail.com>
2022-12-16 08:07:15 +0000

    first commit

---
+++ buildARobotTeen.js
@@ -0,0 +1,11 @@
+// Shut Down Command
+// voice "shut down" / if shut down does not work / fall over - button is pressed that removes the power to remote device.
+// alternatives / special forces prevent the robotTeen from leaving security.
+// real life application at specified locations e.g. a Terminal at an airport location is used for half a day.
+
+// Safety Device
+// "a lead" - for prototypes, such as those used to walk a cat
+// possible rule regarding age<x>weight and strength<x>lifting/pulling_weights / liabiility for injury, e.g., in order for the test to be successful the robotTeen must weigh no more than X and the Human
+// Adult must be able to lift x e.g. dumbell weight and run 5km within 35 minutes.
+
+ 
